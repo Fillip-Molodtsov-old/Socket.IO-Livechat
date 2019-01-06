31eec9fded668262ab17b30e921e63306f5fc4b6
Phillip-V-Molodtsov <40450879+Phillip-V-Molodtsov@users.noreply.github.com>
2019-01-06 18:23:27 +0200

    added Socket.IO

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const PORT = process.env.PORT ||3000;
 
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 const publicPath = path.join(__dirname,'../public');
 app.use(express.static(publicPath))
+
+io.on('connection',(socket)=>{
+    console.log("New user is connected");
+
+    socket.on('disconnect',()=>{
+        console.log("DISCONNECTED");
+    })
+})
 
 app.get('/',(req,res)=>{
     res.sendFile(publicPath+'/html/index.html')
 })
-app.listen(PORT,()=>console.log(`Listening to port ${PORT}`))
+server.listen(PORT,()=>console.log(`Listening to port ${PORT}`))
